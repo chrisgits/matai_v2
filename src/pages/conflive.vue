b96dfa0b523a1af428762909aa1b761f83a27cdc
chrisgits <cmannel77@gmail.com>
2022-09-01 12:10:05 -0500

    updated conf live page

--- src/pages/conflive.vue
+++ src/pages/conflive.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="row justify-center items-start q-gutter-md">
       <q-card class="q-pa-xs" flat>
-        <div class="row text-center">
-          <div class="text-h2 text-center q-mt-sm q-mb-xs">2022 Conference is <span class="text-green text-bold">LIVE!</span></div>
+        <div class="row text-center justify-center">
+          <div class="text-h2 text-center q-mt-sm q-mb-xs"><p>2022 Conference is <span class="text-green text-bold">LIVE!</span></p></div>
           <div class="col-xs-12 col-6">
             <q-img fit="contain" width="20%" class="q-pa-sm q-ma-md" src="../assets/2022_falltrain@6x.png"/>
           </div>
         </div>
         <div class="row">
-          <div class="col-xs-4 col-sm-3 q-pa-md">
-            <q-img fit="contain" width="100%" class="q-pa-sm q-mt-lg" src="../assets/actar.png"/>
+          <div class="col-xs-4 col-sm-3 q-pa-md align-center justify-center">
+            <q-img  class="q-pa-sm q-mt-lg" src="../assets/actar.png"/>
           </div>
           <div class="col-xs-8 col-sm-9 q-pa-md text-h6">
-            <p>Attention!</p>
+            <h5 class="q-ma-none text-bold">Attention!</h5>
             <p>For attendees to receive credit for ACTAR CEUs for this conference you must download the i-Attend app for your device and check in to each session you attend with the QR code available at the entrance. This is a change and process implemented by ACTAR. Please visit <a href="www.actar.org/ceu/earning">www.actar.org/ceu/earning</a> for the details of this change.</p>
             <p>For your convenience here is the <a href="https://northwestern.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=1b0333fe-f7a7-4cbd-a61a-ae900137cf2b">i-Attend Video</a> to the video instructions for the app. If you would like the PDF version download it <a href="">here</a>.</p>
           </div>
@@ -20,46 +20,18 @@
         <q-card-section>
           <div class="text-bold text-h5 text-orange-9">September 12-14, 2022</div>
           <div class="text-h4 q-mt-sm q-mb-xs">Commercial Motor Vehicle Topics</div>
-          <div class="text-h5 q-mt-sm q-mb-xs">Des Moines, IA</div>
-          <br>
-          <h5>Conference Schedule</h5>
-          
+          <div class="text-h5 q-mt-sm q-mb-xs">Des Moines, IA</div>   
         </q-card-section>
   
         <q-card-actions>
           <q-btn
-            href="https://momentum.matai.org/collect/description/248881-q-2022-matai-fall-conference-des-moines-ia" target="blank"
-  
-            unelevated
-            size="lg"
-            color="green"
-            text-color="darkgrey"
-            label="CONF ONLY REGISTRATION"
-          />
-          <q-btn
-            to=/cta
-            unelevated
-            size="lg"
-            color="orange"
-            text-color="darkgrey"
-            label="CTA INFO"
-          />
-          <q-btn
-            href="https://book.rguest.com/onecart/wbe/group/1609/PrairieMeadowsRacetrackandCasino/auth" target="blank"
-            unelevated
-            class="q-my-sm"
-            size="lg"
-            color="teal"
-            label="hotel"
-          />
-          <q-btn
-            href="https://momentum.matai.org/collect/description/257118-q-2022-matai-fall-conference-vendor-registration" target="blank"
+            href="https://files.matai.org/conf_schedule.pdf" target="blank"
             class="q-my-sm"
             unelevated
             size="lg"
             color="blue"
             text-color="white"
-            label="VENDOR REGISTRATION"
+            label="CONF SCHEDULE PDF"
           />
         </q-card-actions>
       </q-card>
@@ -89,63 +61,7 @@
           </q-card>
   
           <br>
-        </q-card-section>
-        <q-card-section>
-          <div class="text-black text-h5">
-            Conference Presenters
-          </div>
-          <br>
-          <q-card  class="bg-grey-8 q-pa-lg"> <!--PRESENTER BIO-->
-            <q-card-section>
-              <div class="text-blue-2 text-h4">
-                Kent Boots – Kent E. Boots & Associates
-              </div>
-              <br>
-              <q-card-section horizontal col-xs-12>
-                <q-card-section class="text-white">
-                  <div class="text-body1 ">
-                    Kent retired from the Orange County Sheriff’s Department (CA) in 2006, and has nearly 30 years of experience in Traffic Collision Investigation and Reconstruction.  He has been involved in Crash Data Retrieval technology for nearly 20 years.  In addition to being a CDR Technician and Analyst, he is a CDR Technician Trainer, and previously co-instructed a CDR Technician Trainer Certification Course.  Kent has also attended additional specialized training to trouble-shoot communication problems and obtain data in situations where other technicians might not be able to.
-                  </div>
-                  <br>
-                  <div class="text-body1">
-                    He has been a guest lecturer on the subject of Crash Data Retrieval numerous times across the United States including the annual EDR Summit held in Texas.  He is also a court recognized expert on Crash Data Retrieval and ignition cycles.  His company provides Crash Data Retrieval consulting services & training as well as selling the CDR Repowering Rig and other related items.  He currently offers a two-day Bosch CDR Tool Operator-Technician course.  He also offers a five-day Advanced Troubleshooting and vehicle systems course.  That course provides the CDR/EDR tool user with an advanced skillset to conduct DLC imaging in more difficult situations where other uses might not be able to.
-                  </div>
-                </q-card-section>
-  
-                <q-img
-                  class="col-5 gt-sm rounded"
-                  src="../assets/img/kent-b2.jpg"
-                />
-              </q-card-section><!--horizontal section-->
-            </q-card-section>
-          </q-card><!--PRESENTER BIO-->
-          <br>
-          <q-card class="bg-grey-8 q-pa-lg"> <!--PRESENTER BIO-->
-            <q-card-section>
-              <div class="text-blue-2 text-h4">
-                William Messerschmidt - Messerschmidt Safety Consultants
-              </div>
-              <br>
-              <q-card-section horizontal>
-                <q-card-section class="text-white">
-                  <div class="text-body1">
-                    William Messerschmidt is a former police officer and traffic homicide investigator. After serving the City of Auburn with distinction for 8 years, Mr. Messerschmidt left government service to work with the late Dr. Edward L. Robinson where his work was focused on the investigation and analysis of commercial and heavy vehicle crashes. While working alongside the physicists and engineers at Robinson & Associates, Bill was introduced to Dr. Jeffery Muttart and almost immediately went back to school for the study of Human Factors Psychology and Engineering.
-                  </div>
-                  <br>
-                  <div class="text-body1">
-                    Since that time, Bill has studied Human Factors, focusing on transportation and industrial safety, at the University of Idaho, Emory-Riddell Aeronautical University, and The University of Aberdeen. Today, Bill’s Accident Reconstruction work is primarily focused on applying Human Factors science to the analysis of traffic crashes and industrial accidents.
-                  </div>
-                </q-card-section>
-  
-                <q-img
-                  class="col-5 gt-sm rounded"
-                  src="../assets/img/bill-m.jpg"
-                />
-              </q-card-section><!--horizontal section-->
-            </q-card-section>
-          </q-card><!--PRESENTER BIO-->
-        </q-card-section>
-  
+        </q-card-section>  
       </q-card>
   
     </div>
