1c0412e98509bc940dc5a76f97114551a99c9db6
C. Mannel <chris@pixelengineering.co>
2022-09-01 22:18:59 -0500

    added conflive and schedule

---
+++ src/components/schedule.vue
@@ -0,0 +1,167 @@
+
+<template>
+  <div class="q-pa-lg">
+    <div class="row q-gutter-md q-mb-sm">
+    </div>
+
+    <q-timeline layout="dense" side="right" color="secondary">
+      <q-timeline-entry heading>CTA Schedule</q-timeline-entry>
+
+      <q-timeline-entry
+        title="Sepember 10th"
+        subtitle="Saturday"
+        side="right"
+        color="orange"
+        icon="engineering"
+      >
+        <div>
+          <h6 class="q-ma-none">08:00a - 12:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Fairgrounds - In Classroom Teaching </h5>
+          <h6 class="q-ma-none">12:00p - 1:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Lunch</h5>
+          <h6 class="q-ma-none">1:00p - 5:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Fairgrounds – Prepping Crash Test Cars </h5>
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="September 11th"
+        subtitle="Sunday"
+        side="right"
+        color="orange"
+        icon="engineering"
+      >
+        <div>
+          <p><span><img style="width:75px; position: relative; top: 5px;" src="../assets/img/usa-flag.png" alt=""></span></p>
+          <h6 class="q-ma-none">08:00a - 12:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Fairgrounds – Continue Prepping Crash Test Cars </h5>
+          <h6 class="q-ma-none">12:00p - 1:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Lunch</h5>
+          <h6 class="q-ma-none">1:00p - 5:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Low Speed Crash Testing – ONLY for Crash Team Academy</h5>
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry heading>Conference Schedule</q-timeline-entry>
+      <q-timeline-entry
+        title="September 11th"
+        subtitle="Sunday"
+        side="right"
+        color="primary"
+      >
+        <div>
+          <p><span><img style="width:75px; position: relative; top: 5px;" src="../assets/img/usa-flag.png" alt=""></span></p>
+          <h6 class="q-ma-none">08:00a - 5:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">ACTAR Testing</h5>
+          <p class="text-bold">Prairie Meadows</p>
+          <h6 class="q-ma-none">07:00p - 8:30p</h6>
+          <h5 class="q-ma-none q-pb-sm">MATAI Board Meeting</h5>
+          <h6 class="q-ma-none">07:00p - 9:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Early Registration (Packet Pickup)</h5>
+          <p class="text-bold">Prairie Meadows Conference Room</p>
+        </div>
+      </q-timeline-entry>
+      <q-timeline-entry
+        title="September 12th"
+        subtitle="Monday"
+        side="right"
+        color="primary"
+      >
+        <div>
+          <h6 class="q-ma-none">7:00a - 7:45a</h6>
+          <h5 class="q-ma-none q-pb-sm">Registration (Packet Pickup)</h5>
+          <p class="text-bold">Prairie Meadows Conference Room</p>
+          <h6 class="q-ma-none">8:00p - 8:45a</h6>
+          <h5 class="q-ma-none q-pb-sm">Opening Ceremonies</h5>
+          <h6 class="q-ma-none">8:45a - 9:15a</h6>
+          <h5 class="q-ma-none q-pb-sm">Transportation to the Iowa State Fairgrounds </h5>
+          <h6 class="q-ma-none">9:15a - 12:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Crash Testing - Fairgrounds</h5>
+          <h6 class="q-ma-none">12:00p - 1:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Lunch - Smoky D's BBQ</h5>
+          <h6 class="q-ma-none">1:00p - 5:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Crash Testing - Fairgrounds</h5>
+          <p class="q-ma-none">Evening</p>
+          <h6 class="q-ma-none">7:00p - 9:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Informal Banquet</h5>
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="September 13th"
+        subtitle="Tuesday"
+        side="left"
+        color="primary"
+      >
+        <div>
+          <h6 class="q-ma-none">8:00a - 9:15a</h6>
+          <h5 class="q-ma-none q-pb-sm">Iowa DOT Enforcement – Logbook data & Equipment Checks</h5>
+          <p class="text-bold">Prairie Meadows Conference Room</p>
+          <h6 class="q-ma-none">9:15a - 9:30a</h6>
+          <h5 class="q-ma-none q-pb-sm">Break</h5>
+          <h6 class="q-ma-none">9:30a - 10:45a</h6>
+          <h5 class="q-ma-none q-pb-sm">Iowa DOT Enforcement (Continued)</h5>
+          <h6 class="q-ma-none">10:45a - 11:00a</h6>
+          <h5 class="q-ma-none q-pb-sm">Break</h5>
+          <h6 class="q-ma-none">11:00a - 12:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">C Robert Rehkemper (Attorney) – Criminal & Civil Discussion on CMVs</h5>
+          <h6 class="q-ma-none">12:00p - 1:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Lunch</h5>
+          <h6 class="q-ma-none">1:00p - 2:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Update of the Autonomous Vehicle & Updates</h5>
+          <h6 class="q-ma-none">2:00p - 2:15p</h6>
+          <h5 class="q-ma-none q-pb-sm">Break</h5>
+          <h6 class="q-ma-none">2:15p - 3:30p</h6>
+          <h5 class="q-ma-none q-pb-sm">Messerschmidt Presentation – CMV Data </h5>
+          <h6 class="q-ma-none">3:30p - 3:45p</h6>
+          <h5 class="q-ma-none q-pb-sm">Break</h5>
+          <h6 class="q-ma-none">3:45p - 5:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Messerschmidt Presentation – CMV Data </h5>
+          <p class="q-ma-none">Evening Activity</p>
+        </div>
+      </q-timeline-entry>
+      <q-timeline-entry
+        title="September 14th"
+        subtitle="Wednesday"
+        side="left"
+        color="primary"
+      >
+        <div>
+          <h6 class="q-ma-none">8:00a - 9:15a</h6>
+          <h5 class="q-ma-none q-pb-sm">Kent Boots Presentation – ACM Data and CDR Operations</h5>
+          <p class="text-bold">Prairie Meadows Conference Room</p>
+          <h6 class="q-ma-none">9:15a - 9:30a</h6>
+          <h5 class="q-ma-none q-pb-sm">Break</h5>
+          <h6 class="q-ma-none">9:30a - 10:45a</h6>
+          <h5 class="q-ma-none q-pb-sm">Kent Boots Presentation (Continued) – ACM Data and CDR Operations</h5>
+          <h6 class="q-ma-none">10:45a - 11:00a</h6>
+          <h5 class="q-ma-none q-pb-sm">Break</h5>
+          <h6 class="q-ma-none">11:00a - 12:00p</h6>
+          <h5 class="q-ma-none q-pb-sm">Review of Testing at the Iowa Speedway </h5>
+          <h6 class="q-ma-none">12:00p - 12:15p</h6>
+          <h5 class="q-ma-none q-pb-sm">Conclusion and upcoming conference announcements</h5>
+        </div>
+      </q-timeline-entry>
+    </q-timeline>
+  </div>
+</template>
+
+<style scoped>
+h6 {
+  font-weight: bold;
+  color: rgba(23, 112, 255, 0.764);
+}
+</style>
+
+<script>
+  import { ref } from 'vue'
+
+  export default {
+    setup () {
+      return {
+        layout: ref('dense'),
+        side: ref('right')
+      }
+    }
+  }
+  </script>
