bbcb89a596f7158af387b0ba2991ba925b339432
chrisgits <chris@pixelengineering.co>
2023-06-19 23:55:42 -0500

    added conf counter back

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="q-px-none">
     <div class="q-mt-none q-pb-lg">
-      <!-- <q-banner dense inline-actions class=" col-xs-12 text-white text-center bg-green q-pa-lg">
-        ADD COUNTER BACK WHEN NEEDED!
+      <q-banner dense inline-actions class=" col-xs-12 text-white text-center bg-green q-pa-lg">
+        <!-- ADD COUNTER BACK WHEN NEEDED! -->
         <Counter/>
-    </q-banner> -->
+    </q-banner>
     <q-banner dense inline-actions class=" col-xs-12 text-white text-center bg-orange q-pa-md">
       <h4 class="q-ma-none text-black">Conf 2023 Dates! September 18 - 20th</h4>
     </q-banner>
@@ -262,7 +262,7 @@ import Counter from '../components/Counter.vue';
 export default defineComponent({
   name: 'Home',
   components: {
-//    Counter
+   Counter
 
   }
 })
