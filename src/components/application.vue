634a0eda621bb7ec637de8623693e99e62b9b919
chrisgits <chris@pixelengineering.co>
2024-10-23 14:34:14 -0500

    new app branch with form data

---
+++ src/components/application.vue
@@ -0,0 +1,160 @@
+<template>
+  <div>
+    <h4>APPLICATION FOR MEMBERSHIP</h4>
+
+    <div>
+      <q-form @submit="submitForm">
+      <q-input v-model="formData.name_first" label="First Name" required />
+      <q-input v-model="formData.name_last" label="Last Name" required />
+      <q-input v-model="formData.email" label="Email" type="email" required />
+      <q-input v-model="formData.home_address" label="Address" required />
+      <q-input v-model="formData.city" label="City" required />
+      <q-input v-model="formData.state" label="State" required />
+      <q-input v-model="formData.zip" label="Zip" required />
+      <q-input mask="### - ### - ####"
+        hint=""
+        v-model="formData.tel_home" label="Phone (Home)" required />
+      <q-input mask="### - ### - ####"
+        hint=""
+        v-model="formData.tel_work" label="Phone (Work)" required />
+      <q-input v-model="formData.org" label="Agency/Organization" required />
+      <q-input v-model="formData.title" label="Position/Title" required />
+      <q-input v-model="formData.empl_dur" label="Length of Employment" required />
+      <h6 class="q-my-md">Crash Courses Attended (if applicable)</h6>
+      <p class="q-mt-lg q-mb-xs text-bold">INTERMEDIATE (or equivalent) COURSE ATTENDANCE</p>
+      <q-input v-model="formData.course_interm_name" label="Intermediate Course Name" />
+      <q-input v-model="formData.course_interm_loc" label="Intermediate Course Location" />
+      <q-input v-model="formData.course_interm_dates" label="Intermediate Course Dates" />
+      <p class="q-mt-lg q-mb-xs text-bold">ADVANCED (or equivalent) COURSE ATTENDANCE</p>
+      <q-input v-model="formData.course_adv_name" label="Advanced Course Name" />
+      <q-input v-model="formData.course_adv_loc" label="Advanced Course Location" />
+      <q-input v-model="formData.course_adv_dates" label="Advanced Course Dates" />
+      <p class="q-mt-lg q-mb-xs text-bold">TECHNICAL (or equivalent) COURSE ATTENDANCE</p>
+      <q-input v-model="formData.course_tech_name" label="Advanced Course Name" />
+      <q-input v-model="formData.course_tech_loc" label="Advanced Course Location" />
+      <q-input v-model="formData.course_tech_dates" label="Advanced Course Dates" />
+      <p class="q-mt-lg q-mb-xs text-bold">RECONSTRUCTION (or equivalent) COURSE ATTENDANCE</p>
+      <q-input v-model="formData.course_tech_name" label="Advanced Course Name" />
+      <q-input v-model="formData.course_tech_loc" label="Advanced Course Location" />
+      <q-input v-model="formData.course_tech_dates" label="Advanced Course Dates" />
+      <p class="q-mt-lg q-mb-xs text-bold">ACTAR</p>
+      <q-select v-model="model" :options="options" label="ACTAR Accredited?" required/>
+      <q-input v-model="formData.actar_number" label="ACTAR Number"/>
+      <p class="q-mt-lg q-mb-xs text-bold text-blue">Please attach a detailed resume and/or additional information regarding your
+        training and experience. Also, include a copy of the certificates of completion for
+        any training listed on the application. Additional certificates for seminars and
+        related activities are welcome.</p>
+      <p class="q-mt-lg q-mb-xs text-bold text-red-10">Please scan all required documents into one PDF to upload with this form.</p>
+      <q-file v-model="formData.pdfFile" label="Upload PDF Document" accept=".pdf" />
+      <p class="text-bold q-my-sm">By submitting this form and the required attachments, I certify the attached information is true and correct to the best of my knowledge.
+        I authorize MATAI, through its representatives, to verify the information provided herein.</p>
+      <q-btn label="Submit" type="submit" color="primary" class="q-ma-sm" />
+    </q-form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { ref } from 'vue'
+
+export default {
+  name: 'application',
+  data() {
+    return {
+      formData: {
+        name_first: '',
+        name_last: '',
+        email: '',
+        home_address: '',
+        city: '',
+        state: '',
+        zip: '',
+        tel_home: '',
+        tel_work: '',
+        org: '',
+        title: '',
+        empl_dur: '',
+        course_interm_loc: '',
+        course_adv_loc: '',
+        course_tech_loc: '',
+        course_recon_loc: '',
+        course_interm_name: '',
+        course_adv_name: '',
+        course_tech_name: '',
+        course_recon_name: '',
+        course_interm_dates: '',
+        course_adv_dates: '',
+        course_tech_dates: '',
+        course_recon_dates: '',
+        actar_status: '',
+        actar_number: '',
+        pdfFile: null,  // Store the uploaded file
+
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+      const scriptURL = 'https://script.google.com/macros/s/AKfycbzCBmpUN6MFJ-LbwAh2QJsjggf8c9EA4NMc-u3IA4Dg1p7S_4eI2GBzZT-rtyVcgZVtHg/exec';
+      const formData = new FormData();
+
+      formData.append('first_name', this.formData.name_first);
+      formData.append('last_name', this.formData.name_last);
+      formData.append('email', this.formData.email);
+      formData.append('address', this.formData.home_address);
+      formData.append('city', this.formData.city);
+      formData.append('state', this.formData.state);
+      formData.append('zip', this.formData.zip);
+      formData.append('home phone', this.formData.tel_home);
+      formData.append('work phone', this.formData.tel_work);
+      formData.append('organization', this.formData.org);
+      formData.append('title', this.formData.title);
+      formData.append('employment length', this.formData.empl_dur);
+      formData.append('Int name', this.formData.course_interm_name);
+      formData.append('Int loc', this.formData.course_interm_loc);
+      formData.append('Int dates', this.formData.course_interm_dates);
+      formData.append('Adv name', this.formData.course_adv_name);
+      formData.append('Adv loc', this.formData.course_adv_loc);
+      formData.append('Adv dates', this.formData.course_adv_dates);
+      formData.append('Tech name', this.formData.course_tech_name);
+      formData.append('Tech loc', this.formData.course_tech_loc);
+      formData.append('Tech dates', this.formData.course_tech_dates);
+      formData.append('Recon name', this.formData.course_recon_name);
+      formData.append('Recon loc', this.formData.course_recon_loc);
+      formData.append('Recon dates', this.formData.course_recon_dates);
+      formData.append('ACTAR Status', this.formData.actar_status);
+      formData.append('ACTAR Number', this.formData.actar_number);
+      formData.append('pdfFile', this.formData.pdfFile); // Attach the PDF file
+
+      fetch(scriptURL, {
+        method: 'POST',
+        body: JSON.stringify(this.formData),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.result === 'success') {
+          this.$q.notify({ type: 'positive', message: 'Form submitted successfully!' });
+        } else {
+          this.$q.notify({ type: 'negative', message: 'Form submission failed!' });
+        }
+      })
+      .catch(error => {
+        this.$q.notify({ type: 'negative', message: 'Error submitting form!' });
+        console.error(error);
+      });
+    }
+  },
+  setup () {
+    return {
+      model: ref(null),
+      options: [
+        'Yes', 'No'
+      ]
+    }
+  }
+}
+</script>
--- src/pages/applicants.vue
+++ src/pages/applicants.vue
@@ -1,14 +1,31 @@
 <template>
   <q-page class="">
-    <h4>MATAI Applicants Page</h4>
-    <p>Welcome to our applicants page. Your website login has been setup. Once your application is approved you will be redirected to the Members section on your next login.</p>
+    <div class="q-pa-md">
+      <h4 class="text-center">MATAI Applicants</h4>
+      <p>Thank you for your interest in joining our organization. Below you will find our electronic application form. Once you have determined that you have the minimum qualifications set forth below, please fill out the form and submit it to our Board of Directors. We have recently changed to an 'all electronic' system for faster processing of your application. If you are unable to submit the form electronically below please reach out to <a href="mailto: admin@matai.org">admin@matai.org</a> for instructions on how to mail it to our secretary directly.</p>
+      <h5 class="text-bold text-center">MEMBERSHIP QUALIFICATION REQUIREMENTS</h5>
+      <ul>
+        <li class="q-mb-sm">Applicants must have successfully completed a recognized <span class="text-bold">Technical Accident
+          Investigation Course</span>, or its equivalent.</li>
+        <li>OR, successfully completed an advanced <span class="text-bold">Accident Investigation Course</span> with at
+        least one (1) year experience in the area of Accident Investigation, and provide a
+        copy of an investigation completed during that year, to include a scaled diagram (either hand-drawn or electronic)</li>
+      </ul>
+    <Application></Application>
+    </div>
+
   </q-page>
 </template>
 
 <script>
+import Application from 'src/components/application.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'About'
+  name: 'Home',
+  components: {
+   Application
+
+  }
 })
 </script>
