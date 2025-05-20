e45b4bbc41ede5dcb89101601146cd5b4dd723b9
chrisgits <chris@pixelengineering.co>
2025-05-19 22:54:31 -0500

    added conf sched pdf and conflive 2025 updates

---
+++ src/pages/conflive2025.vue
@@ -0,0 +1,219 @@
+<template>
+  <q-banner dense inline-actions class=" col-xs-12 text-white text-center bg-green">
+    2025 CONFERENCE
+  </q-banner>
+  <div class="row justify-center items-start q-gutter-md">
+    <q-card class="q-pa-xs" flat>
+      <div class="row text-center justify-center">
+        <!-- <div class="text-h2 text-center q-mt-sm q-mb-xs"><p>2022 Conference is <span class="text-green text-bold">LIVE!</span></p></div> -->
+        <div class="col-xs-12 col-6 lt-sm">
+          <q-img fit="contain" width="100%" class="q-pa-sm q-ma-md" src="../assets/2025_Conf_Splash.jpg"/>
+        </div>
+        <div class="col-xs-12 col-6 gt-xs">
+          <q-img fit="contain" width="100%" class="q-pa-sm q-ma-md" src="../assets/2025_Conf_Logo.png"/>
+        </div>
+      </div>
+      <q-card-section>
+        <h6 class="q-ma-none q-pa-none">Printable Schedule</h6>
+      </q-card-section>
+
+      <q-card-actions class="row justify-center q-ma-none">
+        <q-btn
+          href="https://files.matai.org/public/2025_MATAI_Conf_Schedule.pdf" target="blank"
+          class="q-mb-sm  col-xs-12"
+          unelevated
+          size="lg"
+          color="green"
+          text-color="white"
+          label="CONF SCHEDULE PDF"
+        />
+      </q-card-actions>
+      <div class="row justify-center q-ma-none q-pa-sm">
+          <q-btn
+            href="https://maps.app.goo.gl/2rcW8pr4Sk3FpDP86" target="blank"
+            class="q-ml-none col-xs-12"
+            unelevated
+            size="lg"
+            color="blue"
+            icon="location_on"
+            text-color="white"
+            label="CHULA VISTA HOTEL"
+          />
+      </div>
+    </q-card>
+    <!-- <q-banner dense inline-actions class=" col-xs-12 text-white text-center">
+      <q-img fit="contain" class="q-pa-none q-ma-none" src="../assets/Prairie_Conf_Map.png"/>
+    </q-banner> -->
+    <!-- SCHEDULE COMPONENT -->
+    <ConferenceSchedule :schedule-data="presentations" />
+    <!-- SCHEDULE COMPONENT -->
+
+  </div>
+  <section class="text-center">
+    <h4 class="q-pa-sm q-ma-none">IMPORTANT LOCATIONS</h4>
+    <q-card class="q-pa-md col-xs-12">
+      <h4 class="text-bold">Chula Vista Hotel</h4>
+      <h6 class="q-ma-none">1000 Chula Vista Pkwy, Wisconsin Dells, WI 53965</h6>
+      <h5 class="q-ma-xs">+16086783119</h5>
+      <div class="q-pa-md">
+        <q-list bordered>
+          <q-item>MATAI Conference Contact</q-item>
+          <q-item>Chris Mannel - MATAI VP - 531-444-9348</q-item>
+        </q-list>
+      </div>
+    </q-card>
+    <br>
+  </section>
+  <br><br>
+  <section>
+    <div class="row justify-center" id="actar">
+        <div class="col-xs-4 col-sm-3 q-pa-md">
+          <q-img fit="contain" width="100%" class="q-pa-sm q-mt-lg gt-xs" src="../assets/actar.png"/>
+        </div>
+        <div class="col-xs-12 col-sm-9 q-pa-md text-h6">
+          <h3 class="q-ma-none text-red text-bold">Attention!</h3>
+          <p>For attendees to receive credit for ACTAR CEUs for this conference you <span class="text-bold text-red">MUST</span> download the i-Attend app for your device and check in to <span class="text-bold">EACH</span> session you attend with the QR code available at the entrance. This is a change and process implemented by ACTAR. Please visit <a href="http://www.actar.org/ceu/earning">www.actar.org/ceu/earning</a> for the details of this change.</p>
+          <p>For your convenience here is the <a href="https://northwestern.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=1b0333fe-f7a7-4cbd-a61a-ae900137cf2b">link</a> to the i-Attend video instructions for the app. If you would like the PDF instructions download it <a href="https://files.matai.org/public/i-attend.pdf">here</a>.</p>
+          <h4 class="text-center text-deep-orange text-bold">18 credit hours available IF you login to each session as directed.</h4>
+        </div>
+        <q-card-actions class="col-xs-12 col-sm-6">
+          <q-btn
+            href="https://apps.apple.com/us/app/i-attend-self-check-in/id1421201302" target="blank"
+            class="q-my-sm col-xs-12"
+            unelevated
+            size="lg"
+            color="blue"
+            icon="apple"
+            text-color="white"
+            label="i-ATTEND iOS APP"
+          />
+        </q-card-actions>
+        <q-card-actions class="col-xs-12 col-sm-6">
+          <q-btn
+            href="https://play.google.com/store/apps/details?id=com.tnetic.iselfcheck&hl=en_US&gl=US" target="blank"
+            class="q-my-sm col-xs-12"
+            unelevated
+            size="lg"
+            color="blue"
+            icon="android"
+            text-color="white"
+            label="i-ATTEND APP"
+          />
+        </q-card-actions>
+      </div>
+  </section>
+  <!-- social media links -->
+  <section>
+    <q-card class="q-pa-md">
+      <h5 class="text-bold">SOCIAL MEDIA / LINKS</h5>
+      <q-card-actions class="row justify-center">
+          <q-btn
+            href="https://www.youtube.com/channel/UCvLubrvZf15Zm4-ioYWlYHg" target="blank"
+            class="col-xs-12 col-sm-6"
+            unelevated
+            icon="fab fa-youtube-square fa-2x"
+            size="lg"
+            color="red"
+            text-color="white"
+            label="MATAI YOUTUBE"
+          />
+        </q-card-actions>
+        <q-card-actions class="row justify-center">
+          <q-btn
+            href="https://www.facebook.com/Midwest-Association-of-Technical-Accident-Investigators-1513079988957586/" target="blank"
+            class="col-xs-12 col-sm-6"
+            unelevated
+            icon="fab fa-facebook-square fa-2x"
+            size="lg"
+            color="blue"
+            text-color="white"
+            label="MATAI facebook"
+          />
+        </q-card-actions>
+    </q-card>
+  </section>
+</template>
+
+<style lang="css" scoped>
+
+/* #jumbo {
+  background-image: url("../assets/2024_Conf_Splash.jpg");
+  background-color: #686868;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+} */
+
+.presentation-container {
+  animation: fadeIn 1.5s ease-out;
+}
+
+.presentation-title {
+  font-size: 2rem;
+  font-weight: 700;
+  margin-bottom: 0.5rem;
+  color: #2c3e50;
+}
+
+.presentation-subtitle {
+  font-size: 1.5rem;
+  font-weight: 400;
+  color: #7f8c8d;
+}
+
+@keyframes fadeIn {
+  0% { opacity: 0; transform: translateY(20px); }
+  100% { opacity: 1; transform: translateY(0); }
+}
+
+/* Responsive */
+@media (max-width: 600px) {
+  .presentation-title {
+    font-size: 2rem;
+  }
+  .presentation-subtitle {
+    font-size: 1.2rem;
+  }
+}
+</style>
+
+
+<script setup>
+import { ref } from 'vue';
+import ConferenceSchedule from '../components/ConferenceSchedule.vue';
+
+const presentations = {
+'Monday': [
+  { time: '7:00 AM', title: 'Registration/Breakfast', speaker: 'David Maas', type: 'break' },
+  { time: '8:00 AM', title: 'Opening Ceremony', speaker: '', type: 'break' },
+  { time: '8:15 AM', title: 'Recent Issues in Digital Evidence Warrants', speaker: 'David Maas', type: 'main' },
+  { time: '10:15 AM', title: 'Morning Break', speaker: '', type: 'break' },
+  { time: '10:30 AM', title: 'Integrated Workflow of Drone, Pix4D Photogrammetry and Lidar Scanning Processes in Scene Evidence Collection - Introduction of Blender 3D Software Tool Integration', speaker: 'Chris Starrett \/ Chris Mannel', type: 'main' },
+  { time: '12:30 PM', title: 'LUNCH PROVIDED', speaker: '', type: 'break' },
+  { time: '1:30 PM', title: 'EDR Updates', speaker: 'Jon Northrup', type: 'main' },
+  { time: '3:30 PM', title: 'Afternoon Break', speaker: '', type: 'break'  },
+  { time: '3:45 PM', title: 'Panel Discussion - TECH - FROM SCENE TO COURTROOM - How Does Data Collection Translate to a Prosecutable Case?', speaker: 'AAG Tara Jenswold. AAG Emily Thompson, Chris Mannel, Dave Hallman, Chris Starrett, Jon Northrop', type: 'main' }
+],
+'Tuesday': [
+  { time: '7:00 AM', title: 'Breakfast', speaker: '', type: 'main', type: 'break' },
+  { time: '8:00 AM', title: 'Identifying Counterfeit & Non-OEM Airbags in Crash Investigations with Case Study', speaker: 'Thomas Nichols – Global Brand Protection Specialist, General Motors and Automotive Anti-Counterfeiting Council and Thomas Erdmann – Forensic Specialist Seiler Public Safety', type: 'main' },
+  { time: '10:00 AM', title: 'Morning Break', speaker: '', type: 'break'  },
+  { time: '10:15 AM',title: 'I\'m at the Crash, What Do I Need? Gathering Evidence and Preparing for Potential Prosecution', speaker: 'AAG Tara Jenswold, AAG Emily Thompson, Trooper Tracy Drager – Wisconsin State Patrol and Deputy Tim Dexter - Washington County Sheriff’s Office', type: 'TSRP'},
+  { time: '10:15 AM',title: 'Sketchup 3D Modeling for Crash Recon', speaker: 'MATAI Breakout - Bryce Adams', type: 'MATAI' },
+  { time: '12:30 PM', title: 'LUNCH PROVIDED', speaker: '', type: 'break'  },
+  { time: '1:15 or 2:30 PM', title: 'Video Analysis through Axon Software and Case Study', speaker: 'Special Agent Justin Bender – Wisconsin DCI- and Tilo Voitel – Denver Metro Forensics LLC', type: 'breakout' },
+  { time: '1:15 or 2:30 PM', title: 'It\'s Not Just Texting Anymore: What Can You Get From the Phone?', speaker: 'AAG Tara Jenswold and AAG Emily Thompson and Detective Jeff Nocchi – Eau Claire County Sheriff\’s Office', type: 'breakout' },
+  { time: '1:15 or 2:30 PM', title: 'Using Tech to Leverage Fundamental Evidence Collection at Crash Scenes and Creating Deliverables for Court ', speaker: 'Lt. Hunter Martin, Kentucky State Police', type: 'breakout' },
+  { time: '1:15 or 2:30 PM', title: 'WSP - Avoiding Spoilation of Electronic Evidence in CMV Crashes', speaker: 'Inspector Ryan Schultz and Inspector Colton Fields – Wisconsin State Patrol', type: 'breakout' }
+],
+'Wednesday': [
+  { time: '7:00 AM', title: 'Breakfast', speaker: '', type: 'break' },
+  { time: '8:00 or 9:15 AM', title: 'Electric Vehicle Safety for First Responders', speaker: 'Breakout - Jeff Gross', type: 'breakout' },
+  { time: '8:00 or 9:15 AM', title: 'Drone Comparison, Best Fit Methodology and Current Tech Advances', speaker: 'Adam Johnston -Forensic Specialist Seiler Public Safety', type: 'breakout' },
+  { time: '8:00 or 9:15 AM', title: 'Testimony Training - Tips and Common Challenges, From Basic to Expert', speaker: 'AAGs Tara Jenswold and Emily Thompson', type: 'breakout' },
+  { time: '10:30 AM', title: 'Full Session Closing and Release', speaker: 'MATAI and TSRP Representatives', type: 'main' },
+]
+}
+
+</script>
+
--- src/router/routes.js
+++ src/router/routes.js
@@ -6,33 +6,27 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/Index.vue') },
       { path: '/Contact', component: () => import('pages/Contact.vue') },
-      { path: '/confinfo2022', component: () => import('pages/confinfo2022.vue') },
       { path: '/confinfo', component: () => import('pages/confinfo2025.vue') },
       { path: '/traininginfo', component: () => import('pages/TrainingPage.vue') },
       { path: '/members', component: () => import('pages/Members.vue'), meta: {requiresAuth: false} },
       { path: '/applicants', component: () => import('pages/applicants.vue') },
       { path: '/About', component: () => import('pages/About.vue') },
       { path: '/registerTraining', component: () => import('pages/registerTraining.vue') },
-      { path: '/springtraining', component: () => import('pages/springtraining.vue') },
-      { path: '/2022ConfRegistration', component: () => import('pages/registerConf.vue') },
       { path: '/history', component: () => import('pages/history.vue') },
       { path: '/memberlist', component: () => import('pages/MemberList.vue') },
       { path: '/join', component: () => import('pages/join.vue') },
       { path: '/cta', component: () => import('pages/cta.vue') },
-      { path: '/optics-training', component: () => import('pages/optics-training.vue') },
-      { path: '/conflive', component: () => import('src/pages/conflive2024.vue') },
-      { path: '/conflive24', component: () => import('src/pages/conflive2024.vue') },
-      { path: '/springtrain24', component: () => import('pages/springtraining2024.vue') },
+      { path: '/conflive', component: () => import('src/pages/conflive2025.vue') },
       { path: '/newmemberapp', component: () => import('pages/newApp.vue') }
 
     ]
   },
-  // Always leave this as last one,
+  // Always leave this as last one,src/pages/conflive2025.vue
   // but you can also remove it
-  // {
-  //   path: '/:catchAll(.*)*',
-  //   component: () => import('pages/Error404.vue')
-  // }
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/Error404.vue')
+  }
 ]
 
 export default routes
